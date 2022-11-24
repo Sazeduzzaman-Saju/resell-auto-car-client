33fa99a2b31ea28a2749c14d8c07bf2eea4fd5b5
Sazeduzzaman Saju <sazeduzzaman@yahoo.com>
2022-11-24 11:58:28 +0600

    Home Banner section done

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import useWebTitle from '../../hooks/useWebTItle/useWebTitle';
 import Banner from './Banner/Banner';
+import Welcome from './Welcome/Welcome';
 
 const Home = () => {
     useWebTitle('Home')
     return (
         <div>
             <Banner></Banner>
+            <Welcome></Welcome>
         </div>
     );
 };
